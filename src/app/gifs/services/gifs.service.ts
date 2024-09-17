ad3d51e9d084df1081f77483b44e179ceadd8d3b
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-09-17 05:28:20 +0200

    Añadido api key para gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,6 +6,9 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   
   private _tagsHistory:string[] = [];
+  private apiKey:string = "CdKqYrwthqEIWwA1n56OV0tGeD7bZB1r";
+
+  // api.giphy.com/v1/gifs/search?api_key=CdKqYrwthqEIWwA1n56OV0tGeD7bZB1r&q=valorant&limit=10
 
   constructor() { }
 
@@ -13,7 +16,18 @@ export class GifsService {
     return [...this._tagsHistory]; //Para pasarlo por referencia
   }
 
-  public searchTag(tag:string):void{
+  private organizeHistory(tag:string){
+    tag = tag.toLowerCase();
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);  //Esto hace que se filtre todos los elementos del tag metiendolo en uno nuevo pero sin el que esta buscandose
+    }
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10); //filtro a solo los 10 primeros
+  }
+
+  public searchTag(tag:string):void{
+    if(tag.length == 0) return; //Para que no entre consultas vacias
+
+    this.organizeHistory(tag);
   }
 }
